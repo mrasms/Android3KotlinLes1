313a6307e7d86ed04ee056b4238e69c43dcea5e6
mrasms <shurik96kg@gmail.com>
2022-06-05 22:51:40 +0600

    здесь всё стандартно

---
+++ app/src/main/java/com/example/android3kotlinles1/adapters/clickers/OnItemClick.kt
@@ -0,0 +1,8 @@
+package com.example.android3kotlinles1.adapters.clickers
+
+import com.example.android3kotlinles1.models.Model
+
+interface OnItemClick {
+    fun onItemClickListener(model: Model)
+
+}
---
+++ app/src/main/java/com/example/android3kotlinles1/models/Model.kt
@@ -0,0 +1,3 @@
+package com.example.android3kotlinles1.models
+
+data class Model(val anyText: String)
